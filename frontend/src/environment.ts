3fe802d0d82bdd13a5f5338227208ce248455f37
Pascal Zingg <pazingg@gmail.com>
2017-12-30 10:10:51 +0100

    Prod config

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,3 +1,11 @@
-export var Config = {
+const prod = {
+    restEndpoint: `//weather.binarycake.ch/api`
+};
+
+
+const dev = {
     restEndpoint: `//localhost:5000/api`
 };
+
+// Export configuration parameters according to current env
+export var Config = (process.env.NODE_ENV !== 'production') ? dev : prod;
